87127fe0242b2c239b307edaacbaf58e0a2d8337
timocheu <ccs.timotheoche@gmail.com>
2026-03-10 21:18:52 +0800

    update fzf.vim to fzf.lua

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,22 +1,63 @@
 return {
-  'junegunn/fzf.vim',
-  dependencies = {
-    'junegunn/fzf',
-  },
-  config = function()
-      -- FZF mappings in Lua
-  local opts = { noremap = true, silent = true }
-    vim.keymap.set('n', '<Leader>b', ':Buffers<CR>', opts)
-    vim.keymap.set('n', '<Leader>qq', ':Files<CR>', opts)
-    vim.keymap.set('n', '<Leader>qw', ':GFiles<CR>', opts)
-    vim.keymap.set('n', '<Leader>rg', ':Rg<CR>', opts) -- Uncomment if using :Rg
-    vim.keymap.set('n', '<Leader>ww', ':BLines<CR>', opts)
-    vim.keymap.set('n', "<Leader>'", ':Marks<CR>', opts)
-    vim.keymap.set('n', "<Leader>j", ':Jumps<CR>', opts)
-    vim.keymap.set('n', '<Leader>g', ':Commits<CR>', opts)
-    vim.keymap.set('n', '<Leader>H', ':Helptags<CR>', opts)
-    vim.keymap.set('n', '<Leader>hh', ':History<CR>', opts)
-    vim.keymap.set('n', '<Leader>h:', ':History:<CR>', opts)
-    vim.keymap.set('n', '<Leader>h/', ':History/<CR>', opts)
-  end
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        local fzf = require("fzf-lua")
+        fzf.setup({
+            -- 1. Disable previewers by default for all pickers
+            defaults = {
+                previewer = false,
+            },
+
+            -- 2. Enable it specifically for diagnostics
+            lsp = {
+                diagnostics = {
+                    previewer = "builtin", -- or "bat" if you prefer
+                },
+                -- You can also enable it for references if you change your mind:
+                -- references = { previewer = "builtin" },
+            },
+
+            -- Optional: Global UI settings for the preview window
+            winopts = {
+                preview = {
+                    default  = 'hidden',   -- Keeps it hidden unless the picker overrides it
+                    layout   = 'vertical',
+                    vertical = 'down:45%', -- Preview appears at the bottom
+                }
+            }
+        })
+
+        fzf.register_ui_select()
+
+        -- FZF-Lua mappings
+        local opts = { noremap = true, silent = true }
+
+        -- Navigation
+        vim.keymap.set('n', '<Leader>b', fzf.buffers, opts)
+        vim.keymap.set('n', '<Leader>qq', fzf.files, opts)
+        vim.keymap.set('n', '<Leader>qw', fzf.git_files, opts)
+
+        -- Search
+        vim.keymap.set('n', '<Leader>rg', fzf.live_grep, opts) -- Better than standard Rg as it updates as you type
+        vim.keymap.set('n', '<Leader>ww', fzf.blines, opts)
+
+        -- Neovim Meta
+        vim.keymap.set('n', "<Leader>'", fzf.marks, opts)
+        vim.keymap.set('n', "<Leader>j", fzf.jumps, opts)
+        vim.keymap.set('n', '<Leader>c', fzf.git_commits, opts)
+        vim.keymap.set('n', '<Leader>H', fzf.help_tags, opts)
+
+        -- History
+        vim.keymap.set('n', '<Leader>hh', fzf.oldfiles, opts)
+        vim.keymap.set('n', '<Leader>h:', fzf.command_history, opts)
+        vim.keymap.set('n', '<Leader>h/', fzf.search_history, opts)
+
+
+        -- Find references
+        vim.keymap.set("n", "<leader>r", fzf.lsp_references, opts)
+        vim.keymap.set("n", "<leader>dd", fzf.diagnostics_document, opts)
+        vim.keymap.set("n", "<leader>dw", fzf.diagnostics_workspace, opts)
+    end
 }
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,84 +1,76 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup({
-        registries = {
-          "github:mason-org/mason-registry",
-          "github:Crashdummyy/mason-registry",
-        },
-      })
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" },
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup({
+                registries = {
+                    "github:mason-org/mason-registry",
+                    "github:Crashdummyy/mason-registry",
+                },
+            })
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls" },
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-      vim.lsp.config('lua_ls', { capabilities = capabilities })
+            vim.lsp.config('lua_ls', { capabilities = capabilities })
 
 
 
-      -- ROSLYN (+razor support)
-      -- vim.lsp.config("roslyn", { capabilities = capabilities, })
-      vim.filetype.add {
-        extension = {
-          razor = "razor",
-          cshtml = "razor",
-        },
-      }
-      -- END ROSLYN
+            -- ROSLYN (+razor support)
+            -- vim.lsp.config("roslyn", { capabilities = capabilities, })
+            vim.filetype.add {
+                extension = {
+                    razor = "razor",
+                    cshtml = "razor",
+                },
+            }
+            -- END ROSLYN
 
-      -- LSP functions keymap
-      local opts = { noremap = true, silent = true }
+            -- LSP functions keymap
+            local opts = { noremap = true, silent = true }
 
-      -- Go to definition
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+            -- Go to definition
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 
-      -- Go to implementation
-      vim.keymap.set("n", "<leader>i", vim.lsp.buf.implementation, opts)
+            -- Go to implementation
+            vim.keymap.set("n", "<leader>i", vim.lsp.buf.implementation, opts)
 
-      -- Find references
-      vim.keymap.set("n", "<leader>r", vim.lsp.buf.references, opts)
+            -- Hover documentation
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
-      -- Hover documentation
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+            -- Signature help
+            vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
 
-      -- Signature help
-      vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+            -- Rename symbol
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
-      -- Rename symbol
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+            -- Code action (normal + visual)
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 
-      -- Code action (normal + visual)
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-
-      -- Diagnostic navigation
-      vim.keymap.set("n", "<leader>es", vim.diagnostic.open_float, opts)
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-
-      -- Format
-      -- vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, opts)
-    end
-  },
-  {
-    "rachartier/tiny-inline-diagnostic.nvim",
-    event = "VeryLazy", -- Or `LspAttach`
-    priority = 1000,    -- needs to be loaded in first
-    config = function()
-      require('tiny-inline-diagnostic').setup({
-        preset = "minimal",
-      })
-      vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
-    end
-  }
+            -- Format
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+        end
+    },
+    {
+        "rachartier/tiny-inline-diagnostic.nvim",
+        event = "VeryLazy", -- Or `LspAttach`
+        priority = 1000, -- needs to be loaded in first
+        config = function()
+            require('tiny-inline-diagnostic').setup({
+                preset = "minimal",
+            })
+            vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+        end
+    }
 }
